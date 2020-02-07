25f593f75f6db5d3f9980720ac26563f754052b3
tony <aldon.tony@gmail.com>
2020-02-07 05:57:23 +0100

    Use find-file to follow links in org-mode
    * settings/kb-org.el (org-link-frame-setup):  Use find-file
    to follow links in org-mode.

--- settings/kb-org.el
+++ settings/kb-org.el
@@ -11,6 +11,13 @@
 (setq org-confirm-babel-evaluate 'ta-org-confirm-babel-evaluate)
 (setq system-time-locale "C")
 
+(set-default 'org-link-frame-setup
+ (quote
+	((vm . vm-visit-folder-other-frame)
+	 (vm-imap . vm-visit-imap-folder-other-frame)
+	 (gnus . org-gnus-no-new-news)
+	 (file . find-file)
+	 (wl . wl-other-frame))))
 
 (defhydra hydra-org
   (
