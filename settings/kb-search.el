2ff5c03cb9ddee559d53264b94e6754e8a88ea71
tony <aldon.tony@gmail.com>
2020-04-22 14:38:13 +0200

    Update keybinding iy-go-to-char
    * settings/kb-search.el:  Update keybinding (iy-go-to-char,
    iy-go-to-char-backward).

--- settings/kb-search.el
+++ settings/kb-search.el
@@ -97,8 +97,8 @@ Call command `wdired-finish-edit' if `major-mode' is
 (define-key swiper-map (kbd "M-q") 'minibuffer-keyboard-quit)
 (define-key swiper-map (kbd "M-c") 'swiper-mc)
 
-(key-chord-define-global "ii" 'iy-go-to-char)
-(key-chord-define-global "uu" 'iy-go-to-char-backward)
+(key-chord-define-global "''" 'iy-go-to-char)
+(key-chord-define-global ";;" 'iy-go-to-char-backward)
 
 
 (provide 'kb-search)
