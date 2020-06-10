1b1056a5203a8c076c0c052e3ab8f4c6510d5cf4
tony <tony.aldon.adm@gmail.com>
2020-06-10 16:22:11 +0200

    Advice (magit-status, magit-commit-create)
    * settings/setup-css-mode.el:  Add hook (emmet-use-css-transform).
    * settings/setup-init.el:  Advice (magit-status, magit-commit-create).
    Set variable (save-interprogram-paste-before-kill).  Use (y-or-n-p).

--- settings/setup-init.el
+++ settings/setup-init.el
@@ -20,4 +20,13 @@
 (require 'magit)
 (global-set-key (kbd "C-x g") 'magit-status)
 
+(defadvice magit-status (before ta-magit-status-advice activate)
+	(unless (s-contains-p "magit" (buffer-name)) (delete-other-windows)))
+
+(defadvice magit-commit-create (before ta-magit-commit-create-advice activate)
+	(delete-other-windows))
+
+(setq save-interprogram-paste-before-kill t)
+(defalias 'yes-or-no-p 'y-or-n-p)
+
 (provide 'setup-init)
