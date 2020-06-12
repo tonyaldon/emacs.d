fd71f0332a6e9bf87f0f948e72b1e7ff3602f073
tony <tony.aldon.adm@gmail.com>
2020-06-12 14:18:58 +0200

    Add command in (util-learning.el)
    * settings/util-learning.el:  Add command
    (ta-yas-describe-table-by-namehash).

--- settings/util-learning.el
+++ settings/util-learning.el
@@ -11,4 +11,29 @@
 	(windmove-right)
 	(find-file "~/Documents/learning/key-bindings/_mhtml-mode.el"))
 
+(defun ta-yas-describe-table-by-namehash ()
+  "Small changes on `yas-describe-table-by-namehash'"
+  (interactive)
+  (with-current-buffer (get-buffer-create "*YASnippet Tables by NAMEHASH*")
+    (let ((inhibit-read-only t))
+      (erase-buffer)
+      (insert "YASnippet tables by NAMEHASH: \n")
+      (maphash
+       (lambda (_mode table)
+         (insert (format "\nSnippet table `%s':\n\n" (yas--table-name table)))
+         (maphash
+          (lambda (key _v)
+            (insert (format "   %s --> %s\n" key
+                            (let ((names))
+                              (maphash #'(lambda (k _v)
+                                           (push k names))
+                                       (gethash key (yas--table-hash table)))
+                              names))))
+          (yas--table-hash table)))
+       yas--tables))
+    (view-mode +1)
+    (goto-char 1)
+    (display-buffer (current-buffer))))
+
+
 (provide 'util-learning)
