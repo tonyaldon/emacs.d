d80d9be33cdb0c756f510820a9888d4e298f914d
tony <tony.aldon.adm@gmail.com>
2020-06-22 06:55:20 +0200

    Bump emacs.d version to v1.3.12
    * settings/kb-company.el: Upgrade bindings in (company-active-map).

--- settings/kb-company.el
+++ settings/kb-company.el
@@ -1,14 +1,18 @@
 (require 'kb)
-(require 'company)
+(require 'setup-company)
 
 (define-key company-active-map (kbd ">") 'company-filter-candidates)
-(define-key company-active-map (kbd "]") 'company-other-backend)
-(define-key company-active-map (kbd "<next>") 'company-next-page)
+(define-key company-active-map (kbd "]") 'ta-company-fuzzy-mode-toggle)
+
+(define-key company-active-map (kbd "}") 'company-other-backend)
+(define-key company-active-map (kbd "(") 'company-show-doc-buffer)
+(define-key company-active-map (kbd "<left>") 'ta-company-previous-show-doc)
+(define-key company-active-map (kbd "<right>") 'ta-company-next-show-doc)
+(define-key company-active-map (kbd "<up>") 'company-select-previous-or-abort)
+(define-key company-active-map (kbd "<down>") 'company-select-next-or-abort)
 (define-key company-active-map (kbd "<prior>") 'company-previous-page)
+(define-key company-active-map (kbd "<next>") 'company-next-page)
 (define-key company-active-map (kbd "<tab>") 'company-complete-common)
-(define-key company-active-map (kbd "C-<tab>") 'company-complete-common)
-
-
 
 
 (provide 'kb-company)
