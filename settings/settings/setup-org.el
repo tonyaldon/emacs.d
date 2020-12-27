327c43c03cc43fd33c60ea1c3b0b4a23b0e0f06e
tony <tony.aldon.adm@gmail.com>
2020-12-27 15:32:03 +0100

    Add (ta-org-meta-return) command
    * settings/settings/setup-org.el:  Add (ta-org-meta-return) command.

--- settings/settings/setup-org.el
+++ settings/settings/setup-org.el
@@ -58,6 +58,25 @@ Before doing so, re-align the table if necessary."
       (unless (org-at-table-hline-p)
 			  (org-table-goto-column col)))))
 
+(defun ta-org-meta-return (&optional arg)
+  "Insert a new heading or wrap a region in a table.
+Calls `org-insert-heading', `org-insert-item' or
+`org-table-wrap-region', depending on context.
+
+In table, `org-meta-return' calls `org-table-wrap-region' interactively
+but it DOESN'T PASS the prefix arg.  So using `org-meta-return' in table
+to run `org-table-wrap-region' does't work as expected.
+Below you have a workaround to have full power of `org-table-wrap-region'
+when calling `org-meta-return' in tables."
+  (interactive "P")
+  (org-check-before-invisible-edit 'insert)
+  (or (run-hook-with-args-until-success 'org-metareturn-hook)
+      (if (org-at-table-p)
+					(org-table-wrap-region arg)
+				(call-interactively (cond (arg #'org-insert-heading)
+																	((org-in-item-p) #'org-insert-item)
+																	(t #'org-insert-heading))))))
+
 ;;; Hooks
 
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
