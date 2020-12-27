a81edf3a5dbf1185a25aee39f0474da8d34d63a6
tony <tony.aldon.adm@gmail.com>
2020-12-27 15:30:30 +0100

    Update (avy-keys) variable
    * settings/key-bindings/kb.el:  Update (avy-keys) variable using a
      more readable way.

--- settings/key-bindings/kb.el
+++ settings/key-bindings/kb.el
@@ -10,9 +10,9 @@
 
 (setq avy-highlight-first t)
 (setq avy-style 'at-full)
-(setq avy-keys '(97 117 105 101 121 120 111 119 98 110 102 112 107 116 115
-                    114 113 100 108 106 109 99 103 104 40 41 91 93 60 62 44
-                    59 46 58))
+(setq avy-keys '(?a ?u ?i ?e ?y ?x ?o ?w ?b ?n ?f ?p ?k
+										?t ?s ?r ?q ?d ?l ?j ?m ?c ?g ?h
+										?( ?) ?[ ?] ?< ?> ?, ?\; ?. ?:))
 
 ;;;; hydra
 
