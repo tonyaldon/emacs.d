6295ceebb04638bc1cf90b4ef16b948c0600da71
tony <tony.aldon.adm@gmail.com>
2021-01-06 19:08:20 +0100

    Rename (linux-toggle-laptop-output-visibility --> linux-toggle-laptop-output)
    * settings/packages/linux.el:  Rename.

--- settings/packages/linux.el
+++ settings/packages/linux.el
@@ -4,11 +4,11 @@
 
 ;;; i3
 
-;;;; Toggle laptop output visibility
+;;;; Toggle laptop output
 
 (setq linux-i3-config-file "~/work/settings/i3/.config/i3/config")
 
-(defun linux--laptop-output-visibility-config-position ()
+(defun linux--laptop-output-config-position ()
 	"Return buffer position of the config line responsible for the laptop output visibility.
 
 Return nil, if no laptop output visibility configuration line found."
@@ -19,7 +19,7 @@ Return nil, if no laptop output visibility configuration line found."
 			(beginning-of-line)
 			(point))))
 
-(defun linux-toggle-laptop-output-visibility ()
+(defun linux-toggle-laptop-output ()
   "Toggle on/off the laptop output visibility.
 
 This is done via the i3 configuration file `linux-i3-config-file'.
@@ -30,7 +30,7 @@ thought I've no external webcam."
   (interactive)
 	(with-temp-buffer
 		(insert-file-contents linux-i3-config-file)
-		(let ((laptop-config (linux--laptop-output-visibility-config-position))
+		(let ((laptop-config (linux--laptop-output-config-position))
 					output)
 			(when laptop-config
 				(goto-char laptop-config)
@@ -42,8 +42,6 @@ thought I've no external webcam."
 		(write-region (point-min) (point-max) linux-i3-config-file))
 	(if output (message (concat "Laptop output turned: " output))
 		(message "No 'laptop monitor' config line found in `linux-i3-config-file'")))
-
-;; (global-set-key (kbd "C-c o") 'linux-toggle-laptop-output-visibility)
 
 ;;;; Toggle i3bar visibility
 
@@ -58,8 +56,6 @@ thought I've no external webcam."
 		(if (linux--i3bar-is-hidden-p)
 				(shell-command "i3-msg bar mode dock")
 			(shell-command "i3-msg bar mode invisible"))))
-
-;; (global-set-key (kbd "C-c b") 'linux-toggle-i3bar)
 
 ;; COMMENTS
 ;; (s-blank-p "")
@@ -98,8 +94,6 @@ Note that you have to restart your linux session to see the changes."
 			(write-region (point-min) (point-max) linux-xresources))
 		(if dpi (message (concat "Xft.dpi: " dpi))
 			(message "Neither 96 nor 216 is the DPI in ~/.Xresources file"))))
-
-;; (global-set-key (kbd "C-c d") 'linux-toggle-dpi)
 
 ;;; Footer
 (provide 'linux)
