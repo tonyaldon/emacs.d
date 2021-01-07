e45c2ddc990f7dd05760b362788b676b2e77717e
tony <tony.aldon.adm@gmail.com>
2021-01-07 10:34:04 +0100

    ; fix (linux-toggle-laptop-output)
    problem: (output) variable was bound in the wrong scope.
    
    * settings/packages/linux.el:  Fix (linux-toggle-laptop-output).

--- settings/packages/linux.el
+++ settings/packages/linux.el
@@ -9,15 +9,15 @@
 (setq linux-i3-config-file "~/work/settings/i3/.config/i3/config")
 
 (defun linux--laptop-output-config-position ()
-	"Return buffer position of the config line responsible for the laptop output visibility.
+  "Return buffer position of the config line responsible for the laptop output visibility.
 
 Return nil, if no laptop output visibility configuration line found."
-	(save-excursion
-		(beginning-of-buffer)
-		(when (search-forward "laptop monitor" nil t)
-			(next-line)
-			(beginning-of-line)
-			(point))))
+  (save-excursion
+    (beginning-of-buffer)
+    (when (search-forward "laptop monitor" nil t)
+      (next-line)
+      (beginning-of-line)
+      (point))))
 
 (defun linux-toggle-laptop-output ()
   "Toggle on/off the laptop output visibility.
@@ -28,34 +28,33 @@ Note that you have to restart your linux session to see the changes.
 This is useful when using only my laptop to make Zoom calls,
 thought I've no external webcam."
   (interactive)
-	(with-temp-buffer
-		(insert-file-contents linux-i3-config-file)
-		(let ((laptop-config (linux--laptop-output-config-position))
-					output)
-			(when laptop-config
-				(goto-char laptop-config)
-				(if (looking-at "# ")
-						(progn (delete-char 2)
-									 (setq output "off"))
-					(insert "# ")
-					(setq output "on"))))
-		(write-region (point-min) (point-max) linux-i3-config-file))
-	(if output (message (concat "Laptop output turned: " output))
-		(message "No 'laptop monitor' config line found in `linux-i3-config-file'")))
+  (let (output)
+		(with-temp-buffer
+			(insert-file-contents linux-i3-config-file)
+      (when-let ((laptop-config (linux--laptop-output-config-position)))
+        (goto-char laptop-config)
+        (if (looking-at "# ")
+            (progn (delete-char 2)
+                   (setq output "off"))
+          (insert "# ")
+          (setq output "on")))
+			(write-region (point-min) (point-max) linux-i3-config-file))
+		(if output (message (concat "Laptop output turned: " output))
+			(message "No 'laptop monitor' config line found in `linux-i3-config-file'"))))
 
 ;;;; Toggle i3bar visibility
 
 (defun linux--i3bar-is-hidden-p ()
   "Return t if i3bar is hidden."
-	(s-blank-p (shell-command-to-string "i3-msg -t get_tree | grep '\"class\":\"i3bar\"'")))
+  (s-blank-p (shell-command-to-string "i3-msg -t get_tree | grep '\"class\":\"i3bar\"'")))
 
 (defun linux-toggle-i3bar ()
   "Toggle visibility of i3bar."
   (interactive)
   (let ((inhibit-message t))
-		(if (linux--i3bar-is-hidden-p)
-				(shell-command "i3-msg bar mode dock")
-			(shell-command "i3-msg bar mode invisible"))))
+    (if (linux--i3bar-is-hidden-p)
+        (shell-command "i3-msg bar mode dock")
+      (shell-command "i3-msg bar mode invisible"))))
 
 ;; COMMENTS
 ;; (s-blank-p "")
@@ -77,23 +76,24 @@ for consequence to zoom in everything in the screen.
 
 Note that you have to restart your linux session to see the changes."
   (interactive)
-	(let (dpi)
-		(with-temp-buffer
-			(insert-file-contents linux-xresources)
-			(beginning-of-buffer)
-			(cond
-			 ((search-forward "96" nil t)
-				(delete-char -2)
-				(insert "216")
-				(setq dpi "216"))
-			 ((search-forward "216" nil t)
-				(delete-char -3)
-				(insert "96")
-				(setq dpi "96"))
-			 (t nil))
-			(write-region (point-min) (point-max) linux-xresources))
-		(if dpi (message (concat "Xft.dpi: " dpi))
-			(message "Neither 96 nor 216 is the DPI in ~/.Xresources file"))))
+  (let (dpi)
+    (with-temp-buffer
+      (insert-file-contents linux-xresources)
+      (beginning-of-buffer)
+      (cond
+       ((search-forward "96" nil t)
+        (delete-char -2)
+        (insert "216")
+        (setq dpi "216"))
+       ((search-forward "216" nil t)
+        (delete-char -3)
+        (insert "96")
+        (setq dpi "96"))
+       (t nil))
+      (write-region (point-min) (point-max) linux-xresources))
+    (if dpi (message (concat "Xft.dpi: " dpi))
+      (message "Neither 96 nor 216 is the DPI in ~/.Xresources file"))))
+
 
 ;;; Git (format commit message)
 
