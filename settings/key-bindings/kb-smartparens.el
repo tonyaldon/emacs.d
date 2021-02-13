189b0692b33143503792626251e265a74e045a5c
tony <tony.aldon.adm@gmail.com>
2021-02-13 07:33:45 +0100

    rename and adapt (ta-drag-sexp-to-up, ta-drag-sexp-to-down) commands
    * settings/key-bindings/kb-smartparens.el:  rename and adapt
      (ta-drag-sexp-to-up => ta-sp-drag-backward,
      ta-drag-sexp-to-down => ta-sp-drag-forward) commands.

--- settings/key-bindings/kb-smartparens.el
+++ settings/key-bindings/kb-smartparens.el
@@ -24,24 +24,40 @@
 
 (declare-function 'hydra-lines/body "ext:kb-lines")
 
-(defun ta-drag-sexp-to-up ()
-  "Drag next sexp to the left of the previous sexp.
-
-Work as I want with `sp-navigate-interactive-always-progress-point' set to non-nil value."
+(defun ta-sp-drag-backward ()
+  "Drag sp-sexp at point backward."
   (interactive)
-  (sp-forward-sexp)
-  (sp-backward-sexp)
-  (sp-transpose-sexp)
-  (sp-backward-sexp 2))
+  (let ((delta (- (point) (plist-get (sp-get-thing) :beg))))
+    (cond
+     ((equal (ta-sp-touch) 'left)
+      (sp-transpose-sexp)
+      (sp-backward-sexp 2))
+     ((equal (ta-sp-touch) 'right)
+      (sp-backward-sexp)
+      (sp-transpose-sexp)
+      (sp-previous-sexp))
+     ((equal (ta-sp-touch) 'symbol)
+      (sp-backward-sexp)
+      (sp-transpose-sexp)
+      (sp-backward-sexp 2)
+      (forward-char delta)))))
 
-(defun ta-drag-sexp-to-down ()
-  "Drag next sexp to the left of the previous sexp.
-
-Work as I want with `sp-navigate-interactive-always-progress-point' set to non-nil value."
+(defun ta-sp-drag-forward ()
+  "Drag next sexp to the left of the previous sexp."
   (interactive)
-  (sp-forward-sexp)
-  (sp-transpose-sexp)
-  (sp-backward-sexp))
+  (let ((delta (- (point) (plist-get (sp-get-thing) :beg))))
+    (cond
+     ((equal (ta-sp-touch) 'left)
+      (sp-forward-sexp)
+      (sp-transpose-sexp)
+      (sp-backward-sexp))
+     ((equal (ta-sp-touch) 'right)
+      (sp-transpose-sexp))
+     ((equal (ta-sp-touch) 'symbol)
+      (sp-forward-sexp)
+      (sp-transpose-sexp)
+      (sp-backward-sexp)
+      (forward-char delta)))))
 
 (defun ta-sp-touch ()
   "Indicate if cursor is \"touching\" the right or the left of a sp-sexp.
@@ -145,8 +161,8 @@ If the cursor doesn't touch any sp-sexp, return nil."
   ("x" sp-backward-down-sexp)
   ("a" sp-beginning-of-sexp)
   ("e" sp-end-of-sexp)
-  ("d" ta-drag-sexp-to-up)
-  ("s" ta-drag-sexp-to-down)
+  ("d" ta-sp-drag-backward)
+  ("s" ta-sp-drag-forward)
   ;; parenthese type
   ("$" sp-show-enclosing-pair)
   ("{" sp-wrap-curly)
