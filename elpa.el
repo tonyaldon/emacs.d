8e32f8b051975a3b883d1e71df4149bcc34d56c7
tony <tony.aldon.adm@gmail.com>
2021-06-22 07:52:46 +0200

    remove (elpa.el) file
    * elpa.el:  Remove.

--- elpa.el
+++
@@ -1,5 +0,0 @@
-(require 'package)
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")
-                         ("melpa-stable" . "https://stable.melpa.org/packages/")))
-(package-initialize)
